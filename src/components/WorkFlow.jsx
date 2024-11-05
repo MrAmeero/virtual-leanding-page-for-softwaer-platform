1d8d4dd64c926f32a7aa9b7161d2236ee45b9490
Mr Ameer <ameersshop888@gmail.com>
2024-11-06 02:13:38 +0500

    final code

---
+++ src/components/WorkFlow.jsx
@@ -0,0 +1,31 @@
+import { CheckCircle2 } from 'lucide-react'
+import codeImage from '../assets/code.jpg'
+import React from 'react'
+import { checklistItems } from '../constants'
+
+const WorkFlow = () => {
+  return (
+    <div className='mt-20'>
+      <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide'>Acclerate Your 
+      <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text"> Coding Workflow.</span>
+      </h2>
+      <div className='flex flex-wrap justify-center'>
+      <div className='p-2 x-full lg:w-1/2'>
+      <img src={codeImage} alt="code" />
+      </div>
+      <div className='pt-12 w-full lg:w-1/2'>
+      {checklistItems.map((item,index)=>(
+        <div key={index} className="flex mb-12">
+            <div className="h-10 w-10 text-green-400 bg-neutral-900 p-2 justify-center items-center rounded-full"><CheckCircle2/>
+            </div>
+            <h5 className='mt-1 mb-2 text-xl'>{item.title }</h5>
+            <p className='text-md text-neutral-500 '>{item.description}</p>
+        </div>
+      ))}
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default WorkFlow
